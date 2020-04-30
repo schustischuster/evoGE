9baa0e532bfa5a4981ded9d37339f68dbb3b0bfc
Christoph Schuster <christoph.schuster@slcu.cam.ac.uk>
2020-04-30 06:10:59 +0100

    Add files via upload

--- cisNAT/R/SAS_plots.R
+++ cisNAT/R/SAS_plots.R
@@ -1144,10 +1144,10 @@ scatterDensity <- function(x, y) {
 	df$dens <- col2rgb(x)[1,] + 1L
 
 	cols <- colorRampPalette(c(
-		"#1b30a5","#2C40A1","#254DA9","#1F5AB2","#1867BA","#1274C3","#0D97D2","#09B9E1",
+		"#1E34AE","#1B40B2","#194DB6","#165ABA","#1467BE","#1274C3","#0D97D2","#09B9E1",
 		"#04DBF0","#00FEFF","#11FED3","#22FEA7","#33FE7B","#45FE4F","#68FA44","#8BF739",
 		"#AEF32F","#D1F024","#f4ed1a","#F3DB17","#F2CA15","#F1B812","#F0A710","#EF950E",
-		"#EF840B","#EE7209","#ED6107","#EC4F04","#EB3E02","#eb2d00"))(256)
+		"#F1850B","#F37609","#F56707","#F75805","#FA4903","#FF3100"))(256)
 
 	# Original density color settings
 	# plot_data$col <- densCols(x, y, colramp=colorRampPalette(c("#000099", "#000099", "#00FEFF",
