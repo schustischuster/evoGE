9a905fd2f697ba9e16be4adf49ec6f1b1c2c3411
Christoph Schuster <christoph.schuster@slcu.cam.ac.uk>
2020-05-03 00:23:24 +0100

    Add files via upload

--- exprGenes/R/getExprGenes.R
+++ exprGenes/R/getExprGenes.R
@@ -562,12 +562,14 @@ getExprGenes <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD"),
 		sep=";", dec=".", row.names=FALSE, col.names=TRUE)
 	write.table(repl_corr_df_tpm, file=file.path(out_dir, "output", "expr_genes", fname_repl_corr_tpm), 
 		sep=";", dec=".", row.names=FALSE, col.names=TRUE)
-	write.table(repl_corr_df_counts, file=file.path(out_dir, "output", "expr_genes", fname_repl_corr_counts), 
-		sep=";", dec=".", row.names=FALSE, col.names=TRUE)
+	# Optional write count table
+	# write.table(repl_corr_df_counts, file=file.path(out_dir, "output", "expr_genes", fname_repl_corr_counts), 
+	# 	sep=";", dec=".", row.names=FALSE, col.names=TRUE)
 	write.table(th_genes_tpm, file=file.path(out_dir, "output", "expr_genes", fname_th_genes_repl_tpm), 
 		sep=";", dec=".", row.names=FALSE, col.names=TRUE)
-	write.table(th_genes_counts, file=file.path(out_dir, "output", "expr_genes", fname_th_genes_repl_counts), 
-		sep=";", dec=".", row.names=FALSE, col.names=TRUE)
+	# Optional write count table
+	# write.table(th_genes_counts, file=file.path(out_dir, "output", "expr_genes", fname_th_genes_repl_counts), 
+	# 	sep=";", dec=".", row.names=FALSE, col.names=TRUE)
 
 }
 
