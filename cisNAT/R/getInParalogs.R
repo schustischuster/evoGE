04bb37381a91a5479dd564473f8190f86a0a427e
Christoph Schuster <christoph.schuster@slcu.cam.ac.uk>
2020-07-18 17:46:20 +0100

    Update getInParalogs.R

--- cisNAT/R/getInParalogs.R
+++ cisNAT/R/getInParalogs.R
@@ -44,7 +44,6 @@ getInParalogs <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD")
 
 
 
-
     #------- Process orthoginder data and remove inparalogs them from expression tables --------
 
 
@@ -94,24 +93,37 @@ getInParalogs <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD")
     # Note: it's part of an orthogroup because it contains in at least one of the species some inparalogs
     orth_spec_num_great1 <- orth_spec_num[orth_spec_num$orth_greater_1 > 0,]
     all_inparalog_transcripts <- orth_spec_num_great1$orthogroup
+    all_inparalog_transcripts <-strsplit(as.character(all_inparalog_transcripts),", ")
 
-    # Simplify orthogroup transcript list to set of genes that have inparalogs in selected species
-    all_inparalog_transcripts <-unlist(strsplit(as.character(all_inparalog_transcripts),", "))
+    # A) Create data frame with species orthologs and same row length by adding NA values
+    all_inparalog_transcripts_l <- lapply(all_inparalog_transcripts, `length<-`, max(lengths(all_inparalog_transcripts)))
+    all_inparalog_transcripts_m <- do.call(rbind, all_inparalog_transcripts_l)
+    all_inparalog_genes_m <- gsub("\\..*","", all_inparalog_transcripts_m)
+    orthogroup_genes <- as.data.frame(all_inparalog_genes_m)
+    orthogroup_ID <- as.data.frame(seq(1:nrow(orthogroup_genes)))
+    names(orthogroup_ID) <- "ID_orth"
+    orthogroup_genes <- cbind(orthogroup_ID, orthogroup_genes)
+
+    # B) Simplify orthogroup transcripts to a list inparalogs in selected species
+    all_inparalog_transcripts <-unlist(all_inparalog_transcripts)
     all_inparalog_genes <- gsub("\\..*","", all_inparalog_transcripts)
     all_inparalog_genes <- unique(all_inparalog_genes)
-
     all_inparalog_genes <- as.data.frame(all_inparalog_genes)
     colnames(all_inparalog_genes) <- paste0(species_id, "_in_paralogs", sep="")
-
     
 
 
     #----------------------------------- Write output to csv -----------------------------------
 
+        
+    fname_orth <- sprintf('%s.csv', paste("All_orthogroups", species_id, sep="_"))
+    
+    fname_simpl <- sprintf('%s.csv', paste("All_inparalog_genes", species_id, sep="_"))
 
-    fname <- sprintf('%s.csv', paste("All_inparalog_genes", species_id, sep="_"))
+    write.table(orthogroup_genes, file=file.path(out_dir, "output", "cd_gene_pairs", fname_orth), 
+		sep=";", dec=".", row.names=FALSE, col.names=TRUE)
 
-    write.table(all_inparalog_genes, file=file.path(out_dir, "output", "cd_gene_pairs", fname), 
+    write.table(all_inparalog_genes, file=file.path(out_dir, "output", "cd_gene_pairs", fname_simpl), 
 		sep=";", dec=".", row.names=FALSE, col.names=TRUE)
 
 }
