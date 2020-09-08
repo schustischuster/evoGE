fce71cdbf14e4355cac9b970d7ca3024e0eb0bb8
Christoph Schuster <christoph.schuster@slcu.cam.ac.uk>
2020-09-08 23:48:49 +0100

    Update phylo tree aes

--- orthologs/R/plotPhyloCore.R
+++ orthologs/R/plotPhyloCore.R
@@ -41,11 +41,11 @@ plotPhyloCore <- function(div_times = c("Median", "Estimated")) {
      
      # Make marker species phylogeny plot
 	 png(file = file.path(out_dir, "output", "plots", treename), 
-		width = 2600, height = 3700, res = 800)
+		width = 2800, height = 3700, res = 800)
 	 par(mar = c(1, 1, 0, 1))
 
 	 plot(vert_tree, type = "phylogram", use.edge.length = TRUE, show.node.label = FALSE, 
-	 	edge.width = 2, edge.lty = 1, font = 3, root.edge = FALSE, label.offset = 2, 
+	 	edge.width = 1.5, edge.lty = 1, font = 3, root.edge = FALSE, label.offset = 2, 
 	 	direction = "upwards")
 
 	 add.scale.bar(x = 1, y = 1, cex = 0.8, font = 5, col = "red")
