bdf2607d0e7a0b24b859f0b3677244c9a820cf80
Christoph Schuster <christoph.schuster@slcu.cam.ac.uk>
2020-09-15 03:16:23 +0100

    Add pairwise ortholog plotting function

--- orthologs/R/plotPhyloCore.R
+++ orthologs/R/plotPhyloCore.R
@@ -3,10 +3,10 @@
 # Pairwise TimeTree divergence were obtained from TimeTree (http://www.timetree.org/)
 
 
-#-------------------------------------- Read data tables ---------------------------------------
-
-
 plotPhyloCore <- function(div_times = c("Median", "Estimated")) {
+
+    
+#--------------------------- Make phylogram of DevSeq marker species ---------------------------
 
 
 	if(is.element("Median", div_times)) {
@@ -52,7 +52,159 @@ plotPhyloCore <- function(div_times = c("Median", "Estimated")) {
 
 	 dev.off()
 
+
+
+
+#---------------------- Plot pairwise orthologs for DevSeq marker species ----------------------
+
+
+	 # Read coding and non-coding pairwise_orthologs ATH vs species X
+	 pairwise_ortholog <- read.table(file=file.path(in_dir, "summarized_pairwise_orthologs.csv"), 
+	 	sep=";", dec=".", header=TRUE, stringsAsFactors = FALSE)
+
+
+	 # Reshape data for ggplot2
+	 species <- rep(c(pairwise_ortholog$species[1:6]), times = 3)
+
+	 class <- rep(c("coding", "lncRNA", "circRNA"), each = 6)
+
+	 # divergence times are estimated taxon pair times from TimeTree
+	 # http://www.timetree.org/
+	 if(is.element("Median", div_times)) {
+
+	 	div_time <- rep(c(5.8, 8.5, 27.4, 51, 108, 152), times=3)
+
+	 } else if (is.element("Estimated", div_times)) {
+
+	 	div_time <- rep(c(7.1, 9.4, 25.6, 46, 106, 160), times=3)
+	 }
+
+	 div_time <- as.data.frame(div_time)
+	 colnames(div_time) <- "div_time"
+
+	 expressed <- data.frame(rbind(data.frame(expressed = pairwise_ortholog$protein_coding[1:6]), 
+	 	data.frame(expressed = pairwise_ortholog$lncRNA[1:6]), data.frame(expressed = pairwise_ortholog$circRNA[1:6])))
+
+	 ortholo_data <- data.frame(cbind(species, div_time, class, expressed))
+
+	 ortholo_data$species <- factor(ortholo_data$species)
+	 ortholo_data$class <- factor(ortholo_data$class)
+	 ortholo_data$div_time <- as.numeric(ortholo_data$div_time)
+	 ortholo_data$expressed <- as.numeric(ortholo_data$expressed)
+
+	 coding_lnc <- ortholo_data[1:12,]
+	 circ_lnc <- ortholo_data[7:18,]
+
+
+
+     # Plot number of deduplicated reads for each species
+     plotOrthologs <- function(data) {
+
+     fname <- sprintf('%s.jpg', paste(deparse(substitute(data)), div_times, sep="_"))
+
+     p <- ggplot(data, aes(x = div_time, y = expressed)) + 
+     geom_line(aes(x = div_time), size=0.85) + expand_limits(y=0) + 
+     scale_y_continuous(expand = c(0, 0.15), breaks= pretty_breaks(), 
+		 	labels = function(l) { 
+		 		ifelse(l==0, paste0(round(l/1e3,1)),paste0(round(l/1e3,1),"K"))
+		 	}) + 
+     geom_text(aes(y = expressed * 1.145, label = ""))
+
+     q <- p + facet_wrap( ~ class, scales='free_y', ncol = 2) + 
+     theme_bw() + xlab("Divergence time (Myr)") + ylab("Number of orthologs AT vs X") + 
+     scale_color_manual(values = "gray35") + 
+     theme(
+        strip.text = element_blank(), 
+        strip.background = element_blank(),
+        plot.margin = unit(c(15, 0, 0, 2), "points"),
+        axis.ticks.length = unit(0.15, "cm"),
+        axis.ticks = element_line(colour = "gray15", size = 0.4), 
+        panel.grid.major = element_line(size = 0.5), 
+        panel.grid.minor = element_line(size = 0.3),  
+        axis.title.x = element_text(colour = "black", size=12, 
+        	margin = margin(t = 4.75, r = 0, b = -14, l = 0)), 
+        axis.title.y = element_text(colour = "black", size=12, 
+        	margin = margin(t = 0, r = 8, b = 0, l = 0.125)), 
+        axis.text.x = element_text(colour = "black", size=10.5, 
+        	margin = margin(t = 2, r = 0, b = 3.5, l = 0), vjust = 0.5), 
+        axis.text.y = element_text(colour = "black", size=10.5, margin = margin(t = 0, r = 0.75, b = 0, l = 0)), 
+        legend.position = "bottom",
+        legend.title = element_text(colour = "black", size=12, face ="bold"),
+        legend.text = element_text(size=6.1), 
+        legend.key.size = unit(0.5, "cm"),
+        legend.key.height = unit(0.4, "cm"),
+        legend.background = element_rect(fill = NA),
+        legend.key = element_rect(fill = NA),
+        panel.border = element_rect(colour = "grey70", fill=NA, size=1))
+
+
+     pg <- ggplot(data, aes(x = div_time, y = expressed)) + 
+     geom_line(aes(x = div_time), size=0.55) + expand_limits(y=0) + 
+     scale_y_continuous(expand = c(0, 0.15), breaks= pretty_breaks(), 
+		 	labels = function(l) { 
+		 		ifelse(l==0, paste0(round(l/1e3,1)),paste0(round(l/1e3,1),"K"))
+		 	}) + 
+     geom_text(aes(y = expressed * 1.145, label = ""))
+
+     qg <- pg + facet_grid( ~ class, scales='free') + 
+     theme_bw() + xlab("Divergence time (Myr)") + ylab("Number of orthologs AT vs X") + 
+     scale_color_manual(values = "gray35") + 
+     theme(
+        strip.text.x = element_text(margin = margin(0.2, 0, 0.2, 0, "cm"), size = 12), 
+        strip.text.y = element_text(margin = margin(0, 0.2, 0, 0.2, "cm"), size = 12),
+        strip.background = element_rect(colour="grey70", size=1), 
+        plot.margin = unit(c(15, 0, 0, 2), "points"),
+        axis.ticks.length = unit(0.15, "cm"),
+        panel.grid.major = element_line(size = 0.5), 
+        panel.grid.minor = element_line(size = 0.3),  
+        axis.ticks = element_line(colour = "gray15", size = 0.4), 
+        axis.title.x = element_text(colour = "black", size=12, 
+        	margin = margin(t = 4.75, r = 0, b = -14, l = 0)), 
+        axis.title.y = element_text(colour = "black", size=12, 
+        	margin = margin(t = 0, r = 8, b = 0, l = 0.125)), 
+        axis.text.x = element_text(colour = "black", size=10.5, 
+        	margin = margin(t = 2, r = 0, b = 3.5, l = 0), vjust = 0.5), 
+        axis.text.y = element_text(colour = "black", size=10.5, margin = margin(t = 0, r = 0.75, b = 0, l = 0)),  
+        legend.position = "bottom",
+        legend.title = element_text(colour = "black", size=12, face ="bold"),
+        legend.text = element_text(size=6.1), 
+        legend.key.size = unit(0.5, "cm"),
+        legend.key.height = unit(0.4, "cm"),
+        legend.background = element_rect(fill = NA),
+        legend.key = element_rect(fill = NA),
+        panel.border = element_rect(colour = "grey70", fill=NA, size=1))
+
+
+     gt1 = ggplot_gtable(ggplot_build(q))
+     gt2 = ggplot_gtable(ggplot_build(qg))
+
+     gt1 <- gtable_add_rows(gt1, heights = unit(0.325, 'cm'), pos = 2)
+     gt1 <- gtable_add_grob(gt1, grobs = gt2$grobs[grep('strip-t', gt2$layout$name)], t = 2, l = gt1$layout[grep('strip-t.+1$', gt1$layout$name),]$l)
+
+     gt.side1 = gtable_filter(gt2, 'strip-t-1')
+     gt.side2 = gtable_filter(gt2, 'strip-t-2')
+
+     gt1 = gtable_add_cols(gt1, widths = unit(0.01, 'cm'), pos = -1)
+     gt1 = gtable_add_grob(gt1, zeroGrob(), t = 1, l = ncol(gt1), b=nrow(gt1))
+
+     panel_id <- gt1$layout[grep('panel-.+1$', gt1$layout$name),]
+     gt1 = gtable_add_grob(gt1, gt.side1, t = panel_id$t[1], l = ncol(gt1))
+     gt1 = gtable_add_grob(gt1, gt.side2, t = panel_id$t[2], l = ncol(gt1))
+
+
+     png("NUL")
+
+     ggsave(file = file.path(out_dir, "output", "plots", fname), plot = gt1, 
+     	scale = 1, width = 6.2, height = 2.5, units = c("in"), 
+     	dpi = 800, limitsize = FALSE)
+     }
+
+
+     plotOrthologs(data = coding_lnc)
+     plotOrthologs(data = circ_lnc)
+
 }
+
 
 plotPhyloCore("Estimated")
 plotPhyloCore("Median")
