4bbfc13a7ae090994d789924a4e2944dc113181d
Christoph Schuster <christoph.schuster@slcu.cam.ac.uk>
2020-12-28 20:08:57 +0000

    Update aes in plotTreeLength

--- compAnalysis/R/plotTreeLength.R
+++ compAnalysis/R/plotTreeLength.R
@@ -46,8 +46,9 @@ plotTreeLength <- function() {
     bs_tree_length <- cbind(bs_tree_length, dataset)
     colnames(bs_tree_length) <- c("Organ", "Total_tree_length", "Observed_tree_length", "Order")
     bs_tree_length$Organ <- factor(bs_tree_length$Organ)
-    bs_tree_length$Total_tree_length <- as.numeric(bs_tree_length$Total_tree_length)
     bs_tree_length$Order <- factor(bs_tree_length$Order)
+    bs_tree_length$Total_tree_length <- as.numeric(bs_tree_length$Total_tree_length)
+    bs_tree_length$Observed_tree_length <- as.numeric(bs_tree_length$Observed_tree_length)
 
 
     # Create data tables w/ and wo/ pollen
@@ -62,9 +63,6 @@ plotTreeLength <- function() {
     unique_order <- rep(unique(bs_tree_length$Order), each = 9)
     observed_tree_length <- data.frame(unique_organ, observed_values, unique_order)
     colnames(observed_tree_length) <- c("Organ", "Total_tree_length", "Order")
-    observed_tree_length$Organ <- factor(observed_tree_length$Organ)
-    observed_tree_length$Total_tree_length <- factor(observed_tree_length$Total_tree_length)
-    observed_tree_length$Order <- factor(observed_tree_length$Order)
 
 
     # Create data tables w/ and wo/ pollen
@@ -72,48 +70,45 @@ plotTreeLength <- function() {
     observed_tree_length_pollen <- observed_tree_length[observed_tree_length$Organ == "Pollen", ]
     observed_tree_length_stamen <- observed_tree_length[observed_tree_length$Organ == "Stamen", ]
     observed_tree_length_stamen_pollen <- rbind(observed_tree_length_stamen, observed_tree_length_pollen)
-    observed_tree_length_wo_pollen$Total_tree_length <- as.numeric(as.character(observed_tree_length_wo_pollen$Total_tree_length))
-    observed_tree_length_stamen_pollen$Total_tree_length <- as.numeric(as.character(observed_tree_length_stamen_pollen$Total_tree_length))
 
     bs_tree_length_wo_pollen$Organ <- factor(bs_tree_length_wo_pollen$Organ, levels = unique(bs_tree_length_wo_pollen$Organ))
     observed_tree_length_wo_pollen$Organ <- factor(observed_tree_length_wo_pollen$Organ, levels = unique(observed_tree_length_wo_pollen$Organ))
     bs_tree_length_stamen_pollen$Organ <- factor(bs_tree_length_stamen_pollen$Organ, levels = unique(bs_tree_length_stamen_pollen$Organ))
     observed_tree_length_stamen_pollen$Organ <- factor(observed_tree_length_stamen_pollen$Organ, levels = unique(observed_tree_length_stamen_pollen$Organ))
 
-# Mat="#a63126"
 
     plotTTL <- function(data, data2) {
 
         fname <- sprintf('%s.jpg', paste("Total_tree_length_AT"))
             
         p <- ggplot(data=data, aes(x = Organ, y = Total_tree_length, group = Organ)) + 
-        stat_boxplot(geom ='errorbar', width = 0.5, size=1.0, color="black") + 
-        geom_boxplot(width = 0.85, size = 1.25, fatten = 2, color="black", outlier.shape = NA, 
+        stat_boxplot(geom ='errorbar', width = 0.35, size=1.75, color="black") + 
+        geom_boxplot(width = 0.85, size = 1.75, fatten = 1.5, color="black", outlier.shape = NA, 
             alpha = 1, aes(fill = Organ)) + 
         geom_point(data = data2, position = position_dodge(width=0.75), size = 5, col = "red2", 
             aes(x = Organ, y = Total_tree_length)) + 
         scale_fill_manual(values=c('#6a54a9','#53b0db', '#2c8654', '#96ba37', '#fad819','#e075af', '#f2a72f', '#ed311c')) + 
-        scale_y_continuous(expand = c(0.025, 0)) + 
+        scale_y_continuous(expand = c(0.015, 0)) + 
         scale_x_discrete(labels=c("Root" = "Root", "Hypocotyl" = "Hypocotyl", 
             "Leaf" = "Leaf", "veg_apex" = "Apex veg", "inf_apex" = "Apex inf", 
-            "Flower" = "Flower", "Carpel" = "Carpel", "Stamen" = "Stamen"))
+            "Flower" = "Flower", "Carpel" = "Carpel", "Stamen" = "Stamen"), 
+            expand = c(0.035, 0))
 
-        q <- p + theme_classic() + xlab("Organ") + ylab("Total tree length") + 
+        q <- p + theme_classic() + ylab("Total tree length") + 
         theme(text=element_text(size = 16), 
-            strip.text = element_text(size = 29.5), 
-            strip.text.x = element_text(margin = margin(0.58, 0, 0.58, 0, "cm")), 
-            strip.background = element_rect(colour = 'black', fill = NA, size = 2.5), 
+            strip.text = element_text(size = 34), 
+            strip.text.x = element_text(margin = margin(0.6, 0, 0.6, 0, "cm")), 
+            strip.background = element_rect(colour = 'black', fill = NA, size = 3.25), 
             axis.ticks.length = unit(0.45, "cm"), 
-            axis.ticks = element_line(colour = "black", size = 1.25), 
-            axis.line = element_line(colour = 'black', size = 1.25), 
-            plot.margin = unit(c(0.85, 1.175, 2, 0.4),"cm"), 
-            axis.title.y = element_text(size=30, margin = margin(t = 0, r = 15.2, b = 0, l = 10.8), colour="black", 
+            axis.ticks = element_line(colour = "black", size = 1.75), 
+            axis.line = element_line(colour = 'black', size = 1.75), 
+            plot.margin = unit(c(1, 0.25, 1, 0.25),"cm"), 
+            axis.title.y = element_text(size=34, margin = margin(t = 0, r = 18.5, b = 0, l = 2), colour="black", 
                 face = "bold"), 
-            axis.title.x = element_text(size=30, margin = margin(t = 10, r = 0, b = -15, l = 0), colour="black", 
-                face = "bold"), 
-            axis.text.x = element_text(size=29.5, angle=45, margin = margin(t = -48, b = 48), colour="black", 
-                hjust = 0.99, vjust = 0.45), 
-            axis.text.y = element_text(size=29, angle=0, margin = margin(r = 5.5), colour="black"), 
+            axis.title.x = element_blank(), 
+            axis.text.x = element_text(size=34, angle=45, margin = margin(t = -50, b = 35), colour="black", 
+                hjust = 0.99, vjust = 0.5), 
+            axis.text.y = element_text(size=34, angle=0, margin = margin(r = 5), colour="black"), 
             panel.spacing = unit(0.5, "cm"), 
             panel.grid.major = element_blank(),
             panel.grid.minor.x = element_blank(), 
@@ -124,7 +119,7 @@ plotTreeLength <- function() {
         q <- q + facet_wrap(~ Order, scales = "free", nrow = 1)
 
         ggsave(file = file.path(out_dir, "output", "plots", fname), plot = q, 
-            width = 15, height = 13.5, dpi = 300, units = c("in"), limitsize = FALSE) 
+            width = 15.35, height = 12.25, dpi = 300, units = c("in"), limitsize = FALSE) 
     }
 
     plotTTL(data = bs_tree_length_wo_pollen, data2 = observed_tree_length_wo_pollen)
@@ -136,42 +131,41 @@ plotTreeLength <- function() {
         fname <- sprintf('%s.jpg', paste("Total_tree_length_AT_pollen"))
             
         p <- ggplot(data=data, aes(x = Organ, y = Total_tree_length, group = Organ)) + 
-        stat_boxplot(geom ='errorbar', width = 0.5, size=1.0, color="black") + 
-        geom_boxplot(width = 0.85, size = 1.25, fatten = 2, color="black", outlier.shape = NA, 
+        stat_boxplot(geom ='errorbar', width = 0.35, size=1.75, color="black") + 
+        geom_boxplot(width = 0.9, size = 1.75, fatten = 1.5, color="black", outlier.shape = NA, 
             alpha = 1, aes(fill = Organ)) + 
         geom_point(data = data2, position = position_dodge(width=0.75), size = 5, col = "red2", 
             aes(x = Organ, y = Total_tree_length)) + 
         scale_fill_manual(values=c('#ed311c', '#a63126')) + 
-        scale_y_continuous(expand = c(0.025, 0)) + 
-        scale_x_discrete(labels=c("Stamen" = "Stamen", "Pollen" = "Pollen"))
+        scale_y_continuous(expand = c(0.0185, 0)) + 
+        scale_x_discrete(labels=c("Stamen" = "Stamen", "Pollen" = "Pollen"), expand = c(0.115, 0))
 
-        q <- p + theme_classic() + xlab("Organ") + ylab("Total tree length") + 
+        q <- p + theme_classic() + ylab("Total tree length") + 
         theme(text=element_text(size = 16), 
-            strip.text = element_text(size = 29.5), 
-            strip.text.x = element_text(margin = margin(0.58, 0, 0.58, 0, "cm")), 
-            strip.background = element_rect(colour = 'black', fill = NA, size = 2.5), 
+            strip.text = element_text(size = 34), 
+            strip.text.x = element_text(margin = margin(0.6, 0, 0.6, 0, "cm")), 
+            strip.background = element_rect(colour = 'black', fill = NA, size = 3.25), 
             axis.ticks.length = unit(0.45, "cm"), 
-            axis.ticks = element_line(colour = "black", size = 1.25), 
-            axis.line = element_line(colour = 'black', size = 1.25), 
-            plot.margin = unit(c(0.85, 1.175, 2, 0.4),"cm"), 
-            axis.title.y = element_text(size=30, margin = margin(t = 0, r = 15.2, b = 0, l = 10.8), colour="black", 
+            axis.ticks = element_line(colour = "black", size = 1.75), 
+            axis.line = element_line(colour = 'black', size = 1.75), 
+            plot.margin = unit(c(1, 0.25, 1, 20.5),"cm"), 
+            axis.title.y = element_text(size=34, margin = margin(t = 0, r = 18.5, b = 0, l = 2), colour="black", 
                 face = "bold"), 
-            axis.title.x = element_text(size=30, margin = margin(t = 10, r = 0, b = -15, l = 0), colour="black", 
-                face = "bold"), 
-            axis.text.x = element_text(size=29.5, angle=45, margin = margin(t = -37.5, b = 57.5), colour="black", 
-                hjust = 0.99, vjust = 0.45), 
-            axis.text.y = element_text(size=29, angle=0, margin = margin(r = 5.5), colour="black"), 
+            axis.title.x = element_blank(), 
+            axis.text.x = element_text(size=34, angle=45, margin = margin(t = -40, b = 47.75), colour="black", 
+                hjust = 0.99, vjust = 0.5), 
+            axis.text.y = element_text(size=34, angle=0, margin = margin(r = 5), colour="black"), 
             panel.spacing = unit(0.5, "cm"), 
             panel.grid.major = element_blank(),
             panel.grid.minor.x = element_blank(), 
             panel.grid.minor.y = element_blank(), 
             legend.position = "none", 
-            legend.title = element_blank()) 
+            legend.title = element_blank())  
 
         q <- q + facet_wrap(~ Order, scales = "free", nrow = 1)
 
         ggsave(file = file.path(out_dir, "output", "plots", fname), plot = q, 
-            width = 10.5, height = 13.5, dpi = 300, units = c("in"), limitsize = FALSE) 
+            width = 15.35, height = 12.25, dpi = 300, units = c("in"), limitsize = FALSE) 
     }
 
     plotTTL.SO(data = bs_tree_length_stamen_pollen, data2 = observed_tree_length_stamen_pollen)
