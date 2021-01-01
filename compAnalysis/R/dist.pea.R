3799ff0fb5f84b738576fd5595de37033ad691a6
Christoph Schuster <christoph.schuster@slcu.cam.ac.uk>
2021-01-01 19:47:24 +0000

    Add dist.pea.R - metric pearson distance

---
+++ compAnalysis/R/dist.pea.R
@@ -0,0 +1,26 @@
+# Overwrite pearson distance function from TreeExp2 package with metric pearson distance
+    
+    dist.pea = function (expMat = NULL) {
+
+      object_n <- ncol(expMat)
+      gene_n <- nrow(expMat)
+
+      dis.mat <- matrix(0, nrow = object_n, ncol = object_n)
+
+
+      for (i in 1:(object_n-1)) {
+
+        for (j in (i+1):object_n) {
+
+          dis.mat[j,i] <- sqrt(1/2*(1 - cor(expMat[,i],expMat[,j])))
+
+        }
+
+      }
+
+      #browser()
+      colnames(dis.mat) <- colnames(expMat)
+      rownames(dis.mat) <- colnames(dis.mat)
+      dis.mat  + t(dis.mat)
+
+    }
