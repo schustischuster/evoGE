9165fd51ef1e2f6426df063dccf9dfb74ab46be7
Christoph Schuster <christoph.schuster@slcu.cam.ac.uk>
2021-03-22 03:49:12 +0000

    Update getExprTranscripts.R

--- exprGenes/R/getExprTranscripts.R
+++ exprGenes/R/getExprTranscripts.R
@@ -1,7 +1,7 @@
-# Find expressed genes in each sample replicate using ERCC spike-in information
-# Thresholds: 0 (no ERCC threshold-everything above 0.5 TPM), 0.01 (weakest expressed spike-in), 
-# 0.05 (lowest 5% of expressed spike-ins), 0.1 (lowest 10% of expressed spike-ins)
-# Data input: 1) Expression_data WITH spike-in information
+# Find expressed transcripts in each sample replicate using ERCC spike-in information
+# Thresholds: 0 (no ERCC threshold-everything above 0.5 TPM) OR
+# 0.01, 0.05, 0.1 ERCC (percentiles of detected spike-ins)
+# Data input: Expression_data WITH spike-in information
 # Analysis can be performed on both whole single species datasets (ATH: 132 samples; AL: 36 samples)
 # OR on comparative data sets (27 samples)
 # Input sample tables should have the following format:
@@ -13,9 +13,9 @@
 #------------------- Load packages, set directories and read sample tables ---------------------
 
 
-# Define function to get expressed genes
+# Define function to get expressed transcripts
 
-getExprGenes <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD"), 
+getExprTranscripts <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD"), 
 	experiment = c("single-species", "comparative"), threshold) {
 	
 	# Show error message if no species is chosen
@@ -77,45 +77,38 @@ getExprGenes <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD"),
 
 	# Set GTF input gtf file
     if (is.element("ATH", species)) {
-        genesTPM = file.path(in_dir, "Expression_data", "AT_genes_complete_table_tpm_with_circRNA_sample_names.csv")
-        genesCounts = file.path(in_dir, "Expression_data", "AT_genes_intra_norm_count_mat_vsd_sample_names.csv")
-        genesPtMt = file.path(in_dir, "Expression_data", "AT_Pt_Mt_Orthologs.csv")
+        transcriptsTPM = file.path(in_dir, "Expression_data", "AT_transcripts_complete_table_tpm_with_circRNA_sample_names.csv")
+        transcriptsPtMt = file.path(in_dir, "Expression_data", "AT_Pt_Mt_Orthologs.csv")
         species_id <- "ATH"
 
     } else if (is.element("AL", species)) {
-		genesTPM = file.path(in_dir, "Expression_data", "AL_genes_complete_table_tpm_with_circRNA_sample_names.csv")
-		genesCounts = file.path(in_dir, "Expression_data", "AL_genes_intra_norm_count_mat_vsd_sample_names.csv")
-		genesPtMt = file.path(in_dir, "Expression_data", "AL_Pt_Mt_Orthologs.csv")
+		transcriptsTPM = file.path(in_dir, "Expression_data", "AL_transcripts_complete_table_tpm_with_circRNA_sample_names.csv")
+		transcriptsPtMt = file.path(in_dir, "Expression_data", "AL_Pt_Mt_Orthologs.csv")
 		species_id <- "AL"
 
     } else if (is.element("CR", species)) {
-		genesTPM = file.path(in_dir, "Expression_data", "CR_genes_complete_table_tpm_with_circRNA_sample_names.csv")
-		genesCounts = file.path(in_dir, "Expression_data", "CR_genes_intra_norm_count_mat_vsd_sample_names.csv")
-		genesPtMt = file.path(in_dir, "Expression_data", "CR_Pt_Mt_Orthologs.csv")
+		transcriptsTPM = file.path(in_dir, "Expression_data", "CR_transcripts_complete_table_tpm_with_circRNA_sample_names.csv")
+		transcriptsPtMt = file.path(in_dir, "Expression_data", "CR_Pt_Mt_Orthologs.csv")
 		species_id <- "CR"
 
     } else if (is.element("ES", species)) {
-		genesTPM = file.path(in_dir, "Expression_data", "ES_genes_complete_table_tpm_with_circRNA_sample_names.csv")
-		genesCounts = file.path(in_dir, "Expression_data", "ES_genes_intra_norm_count_mat_vsd_sample_names.csv")
-		genesPtMt = file.path(in_dir, "Expression_data", "ES_Pt_Mt_Orthologs.csv")
+		transcriptsTPM = file.path(in_dir, "Expression_data", "ES_transcripts_complete_table_tpm_with_circRNA_sample_names.csv")
+		transcriptsPtMt = file.path(in_dir, "Expression_data", "ES_Pt_Mt_Orthologs.csv")
 		species_id <- "ES"
 
     } else if (is.element("TH", species)) {
-		genesTPM = file.path(in_dir, "Expression_data", "TH_genes_complete_table_tpm_with_circRNA_sample_names.csv")
-		genesCounts = file.path(in_dir, "Expression_data", "TH_genes_intra_norm_count_mat_vsd_sample_names.csv")
-		genesPtMt = file.path(in_dir, "Expression_data", "TH_Pt_Mt_Orthologs.csv")
+		transcriptsTPM = file.path(in_dir, "Expression_data", "TH_transcripts_complete_table_tpm_with_circRNA_sample_names.csv")
+		transcriptsPtMt = file.path(in_dir, "Expression_data", "TH_Pt_Mt_Orthologs.csv")
 		species_id <- "TH"
 
     } else if (is.element("MT", species)) {
-		genesTPM = file.path(in_dir, "Expression_data", "MT_genes_complete_table_tpm_with_circRNA_sample_names.csv")
-		genesCounts = file.path(in_dir, "Expression_data", "MT_genes_intra_norm_count_mat_vsd_sample_names.csv")
-		genesPtMt = file.path(in_dir, "Expression_data", "MT_Pt_Mt_Orthologs.csv")
+		transcriptsTPM = file.path(in_dir, "Expression_data", "MT_transcripts_complete_table_tpm_with_circRNA_sample_names.csv")
+		transcriptsPtMt = file.path(in_dir, "Expression_data", "MT_Pt_Mt_Orthologs.csv")
 		species_id <- "MT"
 
     } else if (is.element("BD", species)) {
-		genesTPM = file.path(in_dir, "Expression_data", "BD_genes_complete_table_tpm_with_circRNA_sample_names.csv")
-		genesCounts = file.path(in_dir, "Expression_data", "BD_genes_intra_norm_count_mat_vsd_sample_names.csv")
-		genesPtMt = file.path(in_dir, "Expression_data", "BD_Pt_Mt_Orthologs.csv")
+		transcriptsTPM = file.path(in_dir, "Expression_data", "BD_transcripts_complete_table_tpm_with_circRNA_sample_names.csv")
+		transcriptsPtMt = file.path(in_dir, "Expression_data", "BD_Pt_Mt_Orthologs.csv")
 		species_id <- "BD"
     }
 
@@ -127,17 +120,16 @@ getExprGenes <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD"),
 
 
 	# Read expression data
-	all_genes_tpm <- read.table(genesTPM, sep=";", dec=".", header=TRUE, stringsAsFactors=FALSE)
-	colnames(all_genes_tpm)[1] <- "gene_id"
-	all_genes_counts <- read.table(genesCounts, sep=";", dec=".", header=TRUE, stringsAsFactors=FALSE)
-	genes_ptmt <- read.table(genesPtMt, sep=";", dec=".", header=TRUE, stringsAsFactors=FALSE)
+	all_transcripts_tpm <- read.table(transcriptsTPM, sep=";", dec=".", header=TRUE, stringsAsFactors=FALSE)
+	colnames(all_transcripts_tpm)[1] <- "transcript_id"
+	transcripts_ptmt <- read.table(transcriptsPtMt, sep=";", dec=".", header=TRUE, stringsAsFactors=FALSE)
 
 
 	# Format expression data and rename pollen samples
     if ((is.element("ATH", species)) && (is.element("comparative", experiment))) {
 
-		all_genes_tpm <- dplyr::select(all_genes_tpm, c(
-			gene_id, biotype, source, info, 
+		all_transcripts_tpm <- dplyr::select(all_transcripts_tpm, c(
+			transcript_id, biotype, source, info, 
 			root_whole_root_5d_.1.,
 			root_whole_root_5d_.2.,
 			root_whole_root_5d_.3.,
@@ -171,7 +163,7 @@ getExprGenes <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD"),
 
     } else if ((is.element("AL", species)) && (is.element("comparative", experiment))) {
 
-		all_genes_tpm <- dplyr::select(all_genes_tpm, -c(
+		all_transcripts_tpm <- dplyr::select(all_transcripts_tpm, -c(
 			flower_stg11_stamens_8w.10w.25d_.1., 
 			flower_stg11_stamens_8w.10w.25d_.2., 
 			flower_stg11_stamens_8w.10w.25d_.3.,
@@ -187,10 +179,10 @@ getExprGenes <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD"),
 
 
     # Stop function here to allow specific analysis of a single species
-    # return_list <- list("species_id" = species_id, "experiment" = experiment, "all_genes_tpm" = all_genes_tpm, "all_genes_counts" = all_genes_counts, "threshold" = threshold, "genes_ptmt" = genes_ptmt)
+    # return_list <- list("species_id" = species_id, "experiment" = experiment, "all_transcripts_tpm" = all_transcripts_tpm, "threshold" = threshold, "transcripts_ptmt" = transcripts_ptmt)
     # return(return_list)
     # }
-    # return_objects <- getExprGenes(species="ATH", experiment="single-species", 0.05) # read in GTF and expression data for A.thaliana
+    # return_objects <- getExprTranscripts(species="ATH", experiment="single-species", 0.05) # read in GTF and expression data for A.thaliana
     # list2env(return_objects, envir = .GlobalEnv)
 
 
@@ -203,34 +195,34 @@ getExprGenes <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD"),
 
 
     # Remove info column
-    all_genes_tpm <- dplyr::select(all_genes_tpm, -c(info))
+    all_transcripts_tpm <- dplyr::select(all_transcripts_tpm, -c(info))
     
 
 	# Extract ERCC data
-	ERCC <- all_genes_tpm[all_genes_tpm$gene_id %like% "ERCC", ]
+	ERCC <- all_transcripts_tpm[all_transcripts_tpm$biotype %like% "ERCC", ]
 
 
 	if (threshold > 0) {
 
 	   # Get sample-specific TPM threshold
 	   ERCC_cutoff <- cbind(
-		   data.frame(gene_id="TPM_cutoff"), data.frame(biotype="<NA>"), data.frame(source="<NA>"), 
+		   data.frame(transcript_id="TPM_cutoff"), data.frame(biotype="<NA>"), data.frame(source="<NA>"), 
 		   as.data.frame(t(data.frame(
 			TPM_cutoff = apply(ERCC[,4:ncol(ERCC)], 2, function(i)quantile(i[i>0], threshold)))
 		   ))
 	   )
 
 	   # Bind sample-specific threshold to expression table
-	   all_genes_tpm_cutoff <- rbind(all_genes_tpm, ERCC_cutoff)
+	   all_transcripts_tpm_cutoff <- rbind(all_transcripts_tpm, ERCC_cutoff)
 	}
 
 
 	# Set static threshold of 0.5 TPM for "0 ERCC threshold"
 	if (threshold == 0) {
 		th_values <- data.frame(t(rep(0.5, ncol(ERCC)-3)))
-		names(th_values) <- colnames(all_genes_tpm)[4:ncol(all_genes_tpm)]
-		ERCC_cutoff <- data.frame(gene_id="TPM_cutoff", biotype="NA", source="NA", th_values)
-		all_genes_tpm_cutoff <- rbind(all_genes_tpm, ERCC_cutoff)
+		names(th_values) <- colnames(all_transcripts_tpm)[4:ncol(all_transcripts_tpm)]
+		ERCC_cutoff <- data.frame(transcript_id="TPM_cutoff", biotype="NA", source="NA", th_values)
+		all_transcripts_tpm_cutoff <- rbind(all_transcripts_tpm, ERCC_cutoff)
 	}
 
 
@@ -319,7 +311,7 @@ getExprGenes <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD"),
 		return(return_th_list)
 	}
 
-    return_objects_th <- applyThreshold(all_genes_tpm_cutoff)
+    return_objects_th <- applyThreshold(all_transcripts_tpm_cutoff)
     list2env(return_objects_th, envir = .GlobalEnv)
     #* there are two return objects: 
     #* (1) express_data_th = expression data that only contains genes that show in at least one 
@@ -342,7 +334,7 @@ getExprGenes <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD"),
     calculateAvgExpr <- function(df) {
 
 	# Split data frame by sample replicates into a list
-	# then get rowMeans for each subset and bind averaged data to gene_id/biotype/source column
+	# then get rowMeans for each subset and bind averaged data to transcript_id/biotype/source column
 	
 	averaged_replicates <- do.call(cbind, lapply(split.default(df[4:ncol(df)], 
 			rep(seq_along(df), 
@@ -366,93 +358,39 @@ getExprGenes <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD"),
 	repl_names <- substr(repl_names, 1, nchar(repl_names)-2)
 	repl_names <- gsub('^\\.|\\.$', '', repl_names)
 
-	colnames(express_data_th_avg) <- c("gene_id", "biotype", "source", repl_names)
+	colnames(express_data_th_avg) <- c("transcript_id", "biotype", "source", repl_names)
 
 
 	# Check which biotypes are in df
 	express_data_th_avg$biotype[!(duplicated(express_data_th_avg$biotype))]
 
 	
-	# Remove Pt and Mt genes
-	express_data_th_avg <- express_data_th_avg[!(express_data_th_avg$gene_id %in% genes_ptmt$gene_id),]
+	# Remove Pt and Mt transcripts
+	express_data_th_avg <- express_data_th_avg[!(express_data_th_avg$transcript_id %in% transcripts_ptmt$transcript_id),]
 
 
-	# Get number of genes expressed in each sample type
+	# Get number of transcripts expressed in each sample type
 	protein_coding_subset <- subset(express_data_th_avg, biotype=="protein_coding")
-	lnc_intergenic_subset <- subset(express_data_th_avg, biotype=="lnc_intergenic")
-	lnc_antisense_subset <- express_data_th_avg[express_data_th_avg$biotype %like% "antisense", ]
-	circRNA_subset <- subset(express_data_th_avg, biotype=="circRNA")
 
 	
-	# Double-check that no chloroplast and mito genes from coding gene list
+	# Double-check that no chloroplast and mito genes from coding transcripts list
 	`%nlike%` = Negate(`%like%`)
 
 	if ((is.element("ATH", species_id))) { 
 
-		protein_coding_subset <- protein_coding_subset[protein_coding_subset$gene_id %nlike% "ATMG", ]
-		protein_coding_subset <- protein_coding_subset[protein_coding_subset$gene_id %nlike% "ATCG", ]
+		protein_coding_subset <- protein_coding_subset[protein_coding_subset$transcript_id %nlike% "ATMG", ]
+		protein_coding_subset <- protein_coding_subset[protein_coding_subset$transcript_id %nlike% "ATCG", ]
 	}
 
 
 	expr_protein_coding <- colSums(protein_coding_subset > 0)
-	expr_lnc_antisense <- colSums(lnc_antisense_subset > 0)
-	expr_lnc_intergenic <- colSums(lnc_intergenic_subset > 0)
-	expr_circRNA <- colSums(circRNA_subset > 0)
 
 
 	# Create final list of expressed genes per organ/ sample type
-	expressed_genes_th_avg <- rbind(expr_protein_coding, expr_lnc_antisense, expr_lnc_intergenic, expr_circRNA)
-	expressed_genes_th_avg <- subset(expressed_genes_th_avg, select = -c(biotype, source))
-	colnames(expressed_genes_th_avg)[1] <- "total_expressed"
-	biotype <- substring(rownames(expressed_genes_th_avg), 6)
-	expressed_genes_th_avg <- as.data.frame(cbind(biotype, expressed_genes_th_avg))
-
-
-
-
-#--------------- Calculate correlation for biological replicates of each sample ----------------
-
-
-	# Remove ERCC spike-ins from VST count thresholded expression table
-	`%nlike%` = Negate(`%like%`)
-	express_data_th_counts <- all_genes_counts[rownames(all_genes_counts) %nlike% "ERCC", ]
-
-
-	# Get replicate correlation
-	replCorr <- function(df, coefficient=c("spearman", "pearson")) {
-
-		replicate_corr <- do.call(cbind, lapply(split.default(df[1:ncol(df)], #adjust columns
-								rep(seq_along(df), each = 3, length.out = ncol(df))), #adjust columns
-									function(x) {
-									repl_corr <- data.frame(cbind(
-										cor(x[,1],x[,2],method=coefficient),
-										cor(x[,1],x[,3],method=coefficient),
-										cor(x[,2],x[,3],method=coefficient)));
-									repl_corr
-								}
-							))
-
-		return(replicate_corr)
-	}
-
-
-	# Get replicate correlations based on normalized VST counts
-	repl_corr_df_counts <- replCorr(express_data_th_counts, coefficient="pearson")
-	colnames(repl_corr_df_counts) <- colnames(express_data_th_counts)
-
-
-
-
-#-------- Generate replicate expression lists with thresholded genes for each biotype ---------
-
-
-	# For ERCC-thresholded VST count data
-	protein_coding_th_repl_counts <- subset(all_genes_counts, rownames(all_genes_counts) %in% protein_coding_subset$gene_id)
-	lnc_antisense_th_repl_counts <- subset(all_genes_counts, rownames(all_genes_counts) %in% lnc_antisense_subset$gene_id)
-	lnc_intergenic_th_repl_counts <- subset(all_genes_counts, rownames(all_genes_counts) %in% lnc_intergenic_subset$gene_id)
-	circRNA_th_repl_counts <- subset(all_genes_counts, rownames(all_genes_counts) %in% circRNA_subset$gene_id)
-	th_genes_counts <- rbind(protein_coding_th_repl_counts, lnc_antisense_th_repl_counts, lnc_intergenic_th_repl_counts, 
-		circRNA_th_repl_counts)
+	expr_protein_coding_wo_bt <- expr_protein_coding[names(expr_protein_coding) %nlike% "biotype"]
+	expressed_transcripts_th_avg <- expr_protein_coding_wo_bt[names(expr_protein_coding_wo_bt) %nlike% "source"]
+	names(expressed_transcripts_th_avg)[1] <- "total_expressed"
+	expressed_transcripts_th_avg<-as.data.frame(t(expressed_transcripts_th_avg))
 
 
 
@@ -465,25 +403,15 @@ getExprGenes <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD"),
 
 
 	# Set filename
-    fname_expr_genes <- sprintf('%s.csv', paste(species_id, "expr_genes", threshold, sep="_"))
-    fname_repl_corr_counts <- sprintf('%s.csv', paste(species_id, "repl_corr_counts", threshold, sep="_"))
-    fname_th_genes_repl_counts <- sprintf('%s.csv', paste(species_id, "th_genes_repl_counts", threshold, sep="_"))
+    fname_expr_transcripts <- sprintf('%s.csv', paste(species_id, "expr_coding_transcripts", threshold, sep="_"))
 
 
 	# Write final data tables to csv files and store them in /out_dir/output/data_tables
 	if (!dir.exists(file.path(out_dir, "output", "expr_genes"))) 
 		dir.create(file.path(out_dir, "output", "expr_genes"), recursive = TRUE)
 
-	write.table(expressed_genes_th_avg, file=file.path(out_dir, "output", "expr_genes", fname_expr_genes), 
+	write.table(expressed_transcripts_th_avg, file=file.path(out_dir, "output", "expr_genes", fname_expr_transcripts), 
 		sep=";", dec=".", row.names=FALSE, col.names=TRUE)
-	write.table(repl_corr_df_counts, file=file.path(out_dir, "output", "expr_genes", fname_repl_corr_counts), 
-		sep=";", dec=".", row.names=FALSE, col.names=TRUE)
-
-	# Only write thresholded expression tables for 0.5 TPM threshold to file
-	if (threshold == 0) {
-		write.table(th_genes_counts, file=file.path(out_dir, "output", "expr_genes", fname_th_genes_repl_counts), 
-			sep=";", dec=".", row.names=TRUE, col.names=TRUE)
-	}
 
 }
 
@@ -492,12 +420,12 @@ getExprGenes <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD"),
 thresholds <- list(0, 0.01, 0.05, 0.1) # ERCC threshold values are 0 (a fixed TPM threshold of 0.5)
 # or perc of expressed spike-ins for 0.01/0.05/0.1
 
-lapply(thresholds, getExprGenes, species = "ATH", experiment = "single-species")
-lapply(thresholds, getExprGenes, species = "AL", experiment = "comparative")
-lapply(thresholds, getExprGenes, species = "CR", experiment = "comparative")
-lapply(thresholds, getExprGenes, species = "ES", experiment = "comparative")
-lapply(thresholds, getExprGenes, species = "TH", experiment = "comparative")
-lapply(thresholds, getExprGenes, species = "MT", experiment = "comparative")
-lapply(thresholds, getExprGenes, species = "BD", experiment = "comparative")
+lapply(thresholds, getExprTranscripts, species = "ATH", experiment = "single-species")
+lapply(thresholds, getExprTranscripts, species = "AL", experiment = "comparative")
+lapply(thresholds, getExprTranscripts, species = "CR", experiment = "comparative")
+lapply(thresholds, getExprTranscripts, species = "ES", experiment = "comparative")
+lapply(thresholds, getExprTranscripts, species = "TH", experiment = "comparative")
+lapply(thresholds, getExprTranscripts, species = "MT", experiment = "comparative")
+lapply(thresholds, getExprTranscripts, species = "BD", experiment = "comparative")
 
 
