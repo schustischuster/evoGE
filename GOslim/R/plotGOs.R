b93b0ff29081b3a4375e73bbd39e08e1c9c7c01c
Christoph Schuster <christoph.schuster@slcu.cam.ac.uk>
2022-01-29 05:48:37 +0000

    Update col perm+wilcox test stat plot in plotGOs

--- GOslim/R/plotGOs.R
+++ GOslim/R/plotGOs.R
@@ -260,13 +260,13 @@ plotGOs <- function(...) {
         df$goslim_term <- gsub("Embryo development", "Embryo and post-embryonic development", df$goslim_term)
         df$goslim_term <- reorder(df$goslim_term, desc(df$p_value_FDR))
         df$p_value_FDR <- -log(df$p_value_FDR, 10)
-        df$color = ifelse(df$delta_slope < 0, "#5972b5", "#f20000")
+        df$color = ifelse(df$delta_slope < 0, "#4b71ae", "#dc580c")
 
         p <- ggplot(df, aes(x = p_value_FDR, y = goslim_term, colour = color)) +
         geom_point(mapping=aes(size = ortho_genes, colour = color)) + 
         scale_x_continuous(expand = c(0.05, 0), limits = c(2, 3.4), breaks = c(2, 2.5, 3), 
             labels = c("2", "", "3")) + 
-        scale_color_identity(breaks = c("#f20000", "#5972b5"), labels = c("High", "Low"), 
+        scale_color_identity(breaks = c("#dc580c", "#4b71ae"), labels = c("High", "Low"), 
             guide = "legend", name = "Divergence") + 
         scale_size_continuous(range = c(5, 10.55), breaks = c(415, 900, 1600), 
             labels = c("415", "900", "1600"), name = "Gene count") + 
@@ -325,7 +325,7 @@ plotGOs <- function(...) {
         df$goslim_term <- gsub("Embryo development", "Embryo and post-embryonic development", df$goslim_term)
         df$goslim_term <- reorder(df$goslim_term, desc(df$p_value_FDR))
         df$p_value_FDR <- -log(df$p_value_FDR, 10)
-        df$color = ifelse(df$delta_slope < 0, "#5972b5", "#f20000")
+        df$color = ifelse(df$delta_slope < 0, "#4b71ae", "#dc580c")
 
         # Get number of control gene sets
         if (nrow(df) == 9) {
@@ -338,7 +338,7 @@ plotGOs <- function(...) {
         scale_x_continuous(expand = c(0.05, 0), limits = c(2, 5.025), breaks = c(2, 2.5, 3, 3.5, 4, 4.5, 5), 
             labels = c("2", "", "3", "", "4", "", "5")) + 
         scale_y_discrete(expand = c(0.075, 0)) + 
-        scale_color_identity(breaks = c("#f20000", "#5972b5"), labels = c("High", "Low"), 
+        scale_color_identity(breaks = c("#dc580c", "#4b71ae"), labels = c("High", "Low"), 
             guide = "legend", name = "Divergence") + 
         scale_size_continuous(range = c(5.9, 10.25), breaks = c(415, 750, 1100, 1600), 
             labels = c("415", "750", "1100", "1600"), name = "Gene count") + 
