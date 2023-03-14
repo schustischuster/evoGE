d4f1ee99e4195e65318a98cc924a63d755b17e7e
Christoph Schuster <christoph.schuster@slcu.cam.ac.uk>
2023-03-14 02:29:53 +0000

    Rm function calls in getExprGenes

--- exprGenes/R/getExprGenes.R
+++ exprGenes/R/getExprGenes.R
@@ -485,15 +485,3 @@ getExprGenes <- function(species = c("ATH", "AL", "CR", "ES", "TH", "MT", "BD"),
 
 
 
-thresholds <- list(0, 0.01, 0.05, 0.1) # ERCC threshold values are 0 (a fixed TPM threshold of 0.5)
-# or perc of expressed spike-ins for 0.01/0.05/0.1
-
-lapply(thresholds, getExprGenes, species = "ATH", experiment = "single-species")
-lapply(thresholds, getExprGenes, species = "AL", experiment = "comparative")
-lapply(thresholds, getExprGenes, species = "CR", experiment = "comparative")
-lapply(thresholds, getExprGenes, species = "ES", experiment = "comparative")
-lapply(thresholds, getExprGenes, species = "TH", experiment = "comparative")
-lapply(thresholds, getExprGenes, species = "MT", experiment = "comparative")
-lapply(thresholds, getExprGenes, species = "BD", experiment = "comparative")
-
-
