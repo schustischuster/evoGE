c9fc7a04b202738216c6525d6ed56c866c8111a4
Christoph Schuster <christoph.schuster@slcu.cam.ac.uk>
2025-01-29 23:42:28 +0100

    Rm function calls

--- compAnalysis/R/makePhyllogenies.R
+++ compAnalysis/R/makePhyllogenies.R
@@ -512,8 +512,7 @@ makePhyllogenies <- function(expr_estimation = c("TPM", "counts"),
 
 
 
-makePhyllogenies(expr_estimation="counts", coefficient="pearson", devseq_spec="all")
-makePhyllogenies(expr_estimation="counts", coefficient="pearson", devseq_spec="Brassicaceae")
+
 
 
 
